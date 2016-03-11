84ec26e6662289125f2cbf013850674648bc92b2
sarahveness <sarahveness@Sarahs-MacBook-Air-2.local>
2016-03-10 19:39:15 -0800

    Selfiegram!

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selfiegram
+//
+//  Created by Sarah Veness on 2016-03-10.
+//  Copyright © 2016 Sarah Veness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
